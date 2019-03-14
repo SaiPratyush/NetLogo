39000cd4667866e2295411d83983d0f897050f88
charlesres <charly.resendiz@northwestern.edu>
2019-03-14 11:52:05 -0400

    Tests for conflicting versions and world dimensions
    OpenModelTests includes tests for versions and world dimensions after
    serialization/deserialization, along with checking smaller components
    in OpenModel. File extensions are included since the restriction for
    file extensions have been removed.

--- netlogo-gui/src/test/workspace/OpenModelTests.scala
+++ netlogo-gui/src/test/workspace/OpenModelTests.scala
@@ -5,12 +5,12 @@ package org.nlogo.workspace
 import org.scalatest.FunSuite
 
 import java.net.URI
-import java.nio.file.Path
 
-import org.nlogo.core.Model
-import org.nlogo.fileformat.{ defaultConverter, ConversionError, FailedConversionResult, ModelConversion,
-  SuccessfulConversion, ErroredConversion }
+import org.nlogo.core.{ Model, View, WorldDimensions }
+import org.nlogo.api.WorldDimensions3D
+import org.nlogo.fileformat.{ defaultConverter, ModelConversion, NLogoFormat }
 import org.nlogo.api.{ ConfigurableModelLoader, Version }
+import scala.util.Try
 
 class OpenModelTests extends FunSuite {
   val testURI = new URI("file:///foo.test")
@@ -21,150 +21,250 @@ class OpenModelTests extends FunSuite {
     def currentVersion = "NetLogo 6.0"
     def autoconverter: ModelConversion = defaultConverter
 
-    def userContinuesOpen() = controller.openModel(true)
-    def userCancelsOpen() = controller.openModel(false)
-
-    lazy val controller = new MockController()
-    def format = new MockFormat(modelChanges(Model()), None)
+    def format = new NLogoFormat
     object VersionInfo extends Version {
       override def is3D = currentVersion.contains("3D")
       override def knownVersion(v: String) = v == currentVersion || super.knownVersion(v)
     }
-    lazy val loader = new ConfigurableModelLoader().addFormat[String, MockFormat](format)
-    lazy val openedModel = OpenModelFromURI(uri, controller, loader, autoconverter, VersionInfo)
+    lazy val loader = new ConfigurableModelLoader().addFormat[Array[String], NLogoFormat](format)
   }
 
-  test("if asked to open a model will a null path, returns none and reports an invalid URI") { new OpenTest {
-    override val uri = null
-    assert(openedModel.isEmpty)
-    assert(controller.invalidURI == null)
+  test("serializes various version in the model") { new OpenTest {
+    assert(format.version.serialize(new Model) === Array[String]("NetLogo 6.0"))
+    assert(format.version.serialize(new Model(version = "NetLogo 3D 6.0")) ===
+      Array[String]("NetLogo 3D 6.0"))
   } }
 
-  test("if the model doesn't match an available format, notifies the user it is invalid") { new OpenTest {
-    override val uri = new URI("file://foo.jpg")
-    assert(openedModel.isEmpty)
-    assertResult(uri)(controller.invalidURI)
+  test("serializes various dimensions in the model") { new OpenTest {
+    assert((format.interfaceComponent.serialize(new Model)) ===
+      Array("GRAPHICS-WINDOW", "0", "0", "5", "5", "-1", "-1", "12.0",
+        "1", "13", "1", "1", "1", "0", "1", "1", "1", "0", "0", "0",
+        "0", "0", "1", "1", "ticks", "30.0", ""))
+    assert(format.interfaceComponent.serialize(new Model(widgets =
+      List(View(dimensions = new WorldDimensions(0,0,0,0,12.0,true,true))))) ===
+      Array("GRAPHICS-WINDOW", "0", "0", "5", "5", "-1", "-1", "12.0",
+        "1", "13", "1", "1", "1", "0", "1", "1", "1", "0", "0", "0",
+        "0", "0", "1", "1", "ticks", "30.0", ""))
+    assert(format.interfaceComponent.serialize(new Model(widgets =
+      List(View(dimensions = new WorldDimensions3D(0,0,0,0,0,0,12.0,true,true))))) ===
+      Array("GRAPHICS-WINDOW", "0", "0", "5", "5", "-1", "-1", "12.0",
+        "1", "13", "1", "1", "1", "0", "1", "1", "1", "0", "0", "0",
+        "0", "0", "0", "1", "1", "1", "ticks", "30.0", ""))
   } }
 
-  test("if the version doesn't start with NetLogo, notifies the user it is invalid") { new OpenTest {
-    override def modelChanges = _.copy(version = "foobarbaz")
-    val _ = openedModel
-    assertResult(testURI)(controller.invalidURI)
-    assertResult("foobarbaz")(controller.invalidVersion)
+  test("deserializes various dimensions for models") { new OpenTest {
+    val tryModel: Try[Model] = (format.interfaceComponent.deserialize(
+      Array("GRAPHICS-WINDOW", "0", "0", "5", "5", "-1", "-1", "12.0",
+        "1", "13", "1", "1", "1", "0", "1", "1", "1", "0", "0", "0",
+        "0", "1", "1", "1", "ticks", "30.0", ""))(new Model))
+    val tryModel3d: Try[Model] = (format.interfaceComponent.deserialize(
+      Array("GRAPHICS-WINDOW", "0", "0", "5", "5", "-1", "-1", "12.0",
+        "1", "13", "1", "1", "1", "0", "1", "1", "1", "0", "0", "0",
+        "0", "0", "0", "1", "1", "1", "ticks", "30.0", ""))(new Model))
+
+    /* Determines if view is correctly configured */
+    assert(tryModel.get.widgets(0) === View())
+    assert(tryModel.get.widgets(0) ===
+      View(dimensions = new WorldDimensions(0,0,0,0,12.0,true,true)))
+    assert(tryModel3d.get.widgets(0) ===
+      View(dimensions = new WorldDimensions3D(0,0,0,0,0,0,12.0,true,true)))
   } }
 
-  test("if the model is 3D, but NetLogo is open in 2D mode, notifies the user") { new OpenTest {
-    override def modelChanges = _.copy(version = "NetLogo 3D 6.0")
-    userContinuesOpen()
-    assert(openedModel.isDefined)
-    assert(controller.notifiedModelArity == 3)
-    assert(controller.notifiedModelVersion == "NetLogo 3D 6.0")
-    assert(! controller.notifiedVersionUnknown)
+  test("parses sections from provided string") { new OpenTest {
+    val sectionString =
+      """@#$#@#$#@
+      |GRAPHICS-WINDOW
+      |0
+      |0
+      |5
+      |5
+      |-1
+      |-1
+      |12.0
+      |1
+      |13
+      |1
+      |1
+      |1
+      |0
+      |1
+      |1
+      |1
+      |-16
+      |16
+      |-16
+      |16
+      |1
+      |1
+      |1
+      |ticks
+      |30.0
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |NetLogo 6.0.4
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      """.stripMargin
+    val sectionStringWith3D =
+      """@#$#@#$#@
+      |GRAPHICS-WINDOW
+      |0
+      |0
+      |5
+      |5
+      |-1
+      |-1
+      |12.0
+      |1
+      |13
+      |1
+      |1
+      |1
+      |0
+      |1
+      |1
+      |1
+      |-16
+      |16
+      |-16
+      |16
+      |1
+      |1
+      |1
+      |ticks
+      |30.0
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |NetLogo 3D 6.0.4
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      """.stripMargin
+    val section3dString =
+      """@#$#@#$#@
+      |GRAPHICS-WINDOW
+      |0
+      |0
+      |5
+      |5
+      |-1
+      |-1
+      |12.0
+      |1
+      |13
+      |1
+      |1
+      |1
+      |0
+      |1
+      |1
+      |1
+      |-16
+      |16
+      |-16
+      |16
+      |-16
+      |16
+      |1
+      |1
+      |1
+      |ticks
+      |30.0
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |NetLogo 3D 6.0.4
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      """.stripMargin
+    val section3dStringWith2D =
+      """@#$#@#$#@
+      |GRAPHICS-WINDOW
+      |0
+      |0
+      |5
+      |5
+      |-1
+      |-1
+      |12.0
+      |1
+      |13
+      |1
+      |1
+      |1
+      |0
+      |1
+      |1
+      |1
+      |-16
+      |16
+      |-16
+      |16
+      |-16
+      |16
+      |1
+      |1
+      |1
+      |ticks
+      |30.0
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |NetLogo 6.0.4
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      |@#$#@#$#@
+      """.stripMargin
+    val model: Try[Model] = format.load(sectionString,Seq())
+    val model3d: Try[Model] = format.load(section3dString,Seq())
+    val modelWith3dHeader: Try[Model] = format.load(sectionStringWith3D,Seq())
+    val model3dWith2dHeader: Try[Model] = format.load(section3dStringWith2D,Seq())
+
+    /* Model's dimensions after loading */
+    assert(model3d.get.widgets(0) ===
+      View(dimensions = new WorldDimensions3D(-16,16,-16,16,-16,16,12.0,true,true)))
+    assert(model.get.widgets(0) ===
+      View(dimensions = new WorldDimensions(-16,16,-16,16,12.0,true,true)))
+    assert(model3dWith2dHeader.get.widgets(0) ===
+      View(dimensions = new WorldDimensions3D(-16,16,-16,16,-16,16,12.0,true,true)))
+    assert(modelWith3dHeader.get.widgets(0) ===
+      View(dimensions = new WorldDimensions(-16,16,-16,16,12.0,true,true)))
+
+    /* Model's version after loading */
+    assert(model.get.version == "NetLogo 6.0.4")
+    assert(modelWith3dHeader.get.version == "NetLogo 3D 6.0.4")
+    assert(model3d.get.version == "NetLogo 3D 6.0.4")
+    assert(model3dWith2dHeader.get.version == "NetLogo 6.0.4")
   } }
 
-  test("doesn't open different-arity model unless the user approves") { new OpenTest {
-    override def modelChanges = _.copy(version = "NetLogo 3D 6.0")
-    userCancelsOpen()
-    assert(openedModel.isEmpty)
-    assert(controller.notifiedModelArity   == 3)
-    assert(controller.notifiedModelVersion == "NetLogo 3D 6.0")
+  test("determines valid path extensions") { new OpenTest {
+    val acceptedExtNLogo: Option[org.nlogo.api.FormatterPair[_, _]] = loader.uriFormat(new java.net.URI("test.nlogo"))
+    val acceptedExtNLogo3d: Option[org.nlogo.api.FormatterPair[_, _]] = loader.uriFormat(new java.net.URI("test.nlogo3d"))
+    val acceptedExtTest: Option[org.nlogo.api.FormatterPair[_, _]] = loader.uriFormat(new java.net.URI("test.test"))
+
+    /* Compares the format name for equality */
+    assert(acceptedExtNLogo.get.name == format.name)
+    assert(acceptedExtNLogo3d.get.name == format.name)
+    assert(acceptedExtTest.get.name == format.name)
   } }
-
-  test("if the model is in 2D, but NetLogo is open in 3D, notifies the user") { new OpenTest {
-    override def currentVersion = "NetLogo 3D 6.0"
-    userContinuesOpen()
-    assert(openedModel.isDefined)
-    assert(controller.notifiedModelArity   == 2)
-    assert(controller.notifiedModelVersion == "NetLogo 6.0")
-  } }
-
-  test("if the model is not a known version, checks before opening") { new OpenTest {
-    override def modelChanges = _.copy(version = "NetLogo 7.0")
-    userContinuesOpen()
-    assert(openedModel.isDefined)
-    assert(controller.notifiedModelVersion == "NetLogo 7.0")
-  } }
-
-  test("if the model is not a compatible version, checks before opening") { new OpenTest {
-    override def modelChanges = _.copy(version = "NetLogo 4.0")
-    userContinuesOpen()
-    assert(openedModel.isDefined)
-    assert(controller.notifiedModelVersion == "NetLogo 4.0")
-  } }
-
-  test("returns the model if it checks out") { new OpenTest {
-    assertResult(Some(Model()))(openedModel)
-  } }
-
-  test("notifies the user if an error occurs while loading the file") { new OpenTest {
-    val exception = new java.io.IOException("file ain't there")
-    override def format = new MockFormat(modelChanges(Model()), Some(exception))
-    assert(! openedModel.isDefined)
-    assert(controller.notifiedException == exception)
-  } }
-
-  test("runs the autoconversion on the model before returning it") { new OpenTest {
-    override def autoconverter = { (m: Model, p: Path) => SuccessfulConversion(m, m.copy(code = "to foo end")) }
-    assert(openedModel.get.code == "to foo end")
-  } }
-
-  test("notifies the controller if the autoconversion fails") { new OpenTest {
-    val exception = new Exception("problem autoconverting")
-    override def autoconverter = { (m: Model, p: Path) => ErroredConversion(m, ConversionError(exception, "", "")) }
-    userContinuesOpen()
-    assert(openedModel.isDefined)
-    assertResult(Some(Model()))(openedModel)
-    assert(controller.notifiedException == exception)
-  } }
-
-  test("OpenFromSource opens the model properly") { new OpenTest {
-    val modelFromSource = OpenModelFromSource(uri, "model source", controller, loader, autoconverter, VersionInfo)
-    assertResult(Some(Model()))(modelFromSource)
-  } }
-}
-
-class MockController extends OpenModel.Controller {
-  var invalidURI: URI = new java.net.URI("")
-  var invalidVersion: String = _
-  var notifiedModelArity: Int = 0
-  var notifiedModelVersion: String = _
-  var notifiedException: Exception = _
-  var notifiedVersionUnknown: Boolean = false
-  var willOpenModel = false
-
-  def openModel(willDo: Boolean): MockController = {
-    willOpenModel = willDo
-    this
-  }
-
-  def errorOpeningURI(uri: URI, exception: Exception): Unit = {
-    invalidURI = uri
-    notifiedException = exception
-  }
-
-  def errorAutoconvertingModel(res: FailedConversionResult): Option[Model] = {
-    notifiedException = res.errors.head.errors.head
-    if (willOpenModel) Some(res.model)
-    else None
-  }
-
-  def invalidModel(uri: URI): Unit = { invalidURI = uri }
-  def invalidModelVersion(uri: URI, version: String): Unit = {
-    invalidURI = uri
-    invalidVersion = version
-  }
-  def shouldOpenModelOfDifferingArity(arity: Int, version: String): Boolean = {
-    notifiedModelArity = arity
-    notifiedModelVersion = version
-    willOpenModel
-  }
-  def shouldOpenModelOfUnknownVersion(version: String): Boolean = {
-    notifiedModelVersion = version
-    notifiedVersionUnknown = true
-    willOpenModel
-  }
-  def shouldOpenModelOfLegacyVersion(version: String): Boolean = {
-    notifiedModelVersion = version
-    willOpenModel
-  }
 }
